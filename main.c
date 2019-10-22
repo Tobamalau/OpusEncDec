23d4e3024a0e164dfe68d260ac0b7e66e28203d7
Tobias Rist <tobias.rist@preh.de>
2019-10-22 07:52:06 +0200

    bereinigung

--- main.c
+++ main.c
@@ -3,7 +3,8 @@
 #include <string.h>
 #include "opus.h"
 #include <stdio.h>
-#include "marioTestenc.c"
+//#include "marioTestenc.c"
+#include "opusFile.c"
 
 /*The frame size is hardcoded for this sample code but it d../../../../usr/local/include/opusoesn't have to be*/
 #define FRAME_SIZE 960
@@ -16,35 +17,35 @@
 #define MAX_FRAME_SIZE (6*960)
 #define MAX_PACKET_SIZE (3*1276)
 
-int main(int argc, char **argv)
-{
-   opus_int16 in[FRAME_SIZE*CHANNELS];
+struct opusfile {
+   OpusDecoder *decoder;
+   int nbBytes;
+   int max_frame_size;
+   int framesize;
+   unsigned char *input;
+   unsigned int inputLenpth;
    opus_int16 out[MAX_FRAME_SIZE*CHANNELS];
    unsigned char cbits[MAX_PACKET_SIZE];
+} OpusInstanz = {NULL, NBBYTES, MAX_FRAME_SIZE, 0, NULL, MAX_PACKET_SIZE, NULL, NULL};
+
+
+int_fast8_t decodeOpus(struct opusfile *lib)
+{
+   lib->framesize = opus_decode(lib->decoder, lib->cbits, lib->nbBytes, lib->out, lib->max_frame_size, 0);
+   if (lib->framesize<0)
+   {
+    fprintf(stderr, "decoder failed: %s\n", opus_strerror(lib->framesize));
+    return EXIT_FAILURE;
+   }
+   return EXIT_SUCCESS;
+}
+
+
+int main(int argc, char **argv)
+{
    /*Holds the state of the encoder and decoder */
    int err;
-
-   struct opusfile {
-      OpusDecoder *decoder;
-      int nbBytes;
-      int max_frame_size;
-      unsigned char *input;
-      unsigned int inputLenpth;
-      opus_int16 out[MAX_FRAME_SIZE*CHANNELS];
-      unsigned char cbits[MAX_PACKET_SIZE];
-   } OpusInstanz = {NULL, NBBYTES, MAX_FRAME_SIZE, NULL, MAX_PACKET_SIZE, NULL, NULL};
-
-
-   int_fast8_t decodeOpus(struct opusfile *lib)
-   {
-      int frame_size = opus_decode(lib->decoder, lib->input, lib->nbBytes, lib->out, lib->max_frame_size, 0);
-      if (frame_size<0)
-      {
-       fprintf(stderr, "decoder failed: %s\n", opus_strerror(frame_size));
-       return EXIT_FAILURE;
-      }
-      return EXIT_SUCCESS;
-   }
+   uint loopcnt =0;
 
    OpusInstanz.decoder = opus_decoder_create(SAMPLE_RATE, CHANNELS, &err);
    if (err<0)
@@ -53,18 +54,35 @@ int main(int argc, char **argv)
       return EXIT_FAILURE;
    }
 
+
+   OpusInstanz.input = marioTestenc_opus;
    while (1)
    {
-      OpusInstanz.input = &marioTestenc_opus;
-      for(int i=0;i<NBBYTES;i++)
+
+
+      if(marioTestenc_opus_len > NBBYTES*loopcnt)
       {
-         OpusInstanz.cbits[i] = marioTestenc_opus[i];
+         OpusInstanz.input = OpusInstanz.input + (NBBYTES*loopcnt);
+         for(int i=0;i<NBBYTES;i++)
+         {
+            OpusInstanz.cbits[i] = marioTestenc_opus[i + (NBBYTES*loopcnt)];
+         }
+      }
+      else
+         loopcnt = 0;
+
+      decodeOpus(&OpusInstanz);
+      unsigned char pcm_bytes[MAX_FRAME_SIZE*CHANNELS*2];
+      /* Convert to little-endian ordering. */
+      for(int i=0;i<CHANNELS*OpusInstanz.framesize;i++)
+      {
+         pcm_bytes[2*i]=OpusInstanz.out[i]&0xFF;
+         pcm_bytes[2*i+1]=(OpusInstanz.out[i]>>8)&0xFF;
+         printf("\n%o\t%o",pcm_bytes[2*i], pcm_bytes[2*i+1]);
       }
 
 
-      decodeOpus(&OpusInstanz);
-
-
+      loopcnt++;
    }
    /*Destroy the encoder state*/
    opus_decoder_destroy(OpusInstanz.decoder);
